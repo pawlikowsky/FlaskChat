0c2a0e5c87ee4da9b556efa2123e90e4375f5713
ckp1701 <euo886@gmail.com>
2017-07-11 21:30:38 +0200

    socketio improved

--- flaskchat.py
+++ flaskchat.py
@@ -6,15 +6,11 @@ from models import Chat, User, db
 from flask_socketio import SocketIO, join_room, leave_room, send, emit
 
 
-
 app = Flask(__name__)
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 app.username = 'admin'
 app.password = 'password'
 socketio = SocketIO(app)
-
-
-
 
 
 @app.teardown_appcontext
@@ -27,7 +23,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit() and request.method == 'POST':
         chat_uid = ''.join(choice(ascii_letters) for i in range(12))
-        room = Chat(chat_uid,form.language.data)
+        room = Chat(chat_uid, form.language.data)
         user = User(form.username.data)
         db.session.add(room)
         db.session.add(user)
@@ -60,10 +56,10 @@ def join(chat_uid):
         session['chat_uid'] = chat_uid
         return redirect(url_for('czat', chat_uid=chat_uid))
     return render_template('join.html', form=form, chat_uid=chat_uid)
-    
+
 
 @app.route('/id/<chat_uid>', methods=['GET', 'POST'])
-def czat(chat_uid): 
+def czat(chat_uid):
     if not session.get('logged_in'):
         return redirect(url_for('join', chat_uid=chat_uid))
     user = session['username']
@@ -79,7 +75,7 @@ def czat(chat_uid):
     #     q.content = data
     #     db.session.commit()
     #     print('workspace added')
-    return render_template('workspace.html', chat_uid=chat_uid, user=user, form=form, content=content, editor=editor )
+    return render_template('workspace.html', chat_uid=chat_uid, user=user, form=form, content=content, editor=editor)
 
 
 @app.route('/id/<chat_uid>/add', methods=['GET', 'POST'])
@@ -93,13 +89,31 @@ def czat_add(chat_uid):
         print('workspace added')
         return jsonify(data={'message': 'hello {}'.format(form.workspace.data)})
     return jsonify(data=form.errors)
-    
+
+
 @app.route('/id/<chat_uid>/workspace', methods=['GET', 'POST'])
 def czat_workspace(chat_uid):
     q = Chat.query.filter_by(chat_uid=chat_uid).first()
     workspace = q.content
     return jsonify(data={'workspace': workspace})
 
+
+@socketio.on('joined', namespace='/chat')
+def joined(message):
+    room = session.get('chat_uid')
+    join_room(room)
+    emit('status', {'msg': session.get('username') + ' connected'}, room=room)
+
+@socketio.on('text', namespace='/chat')
+def text(message):
+    room = session.get('chat_uid')
+    emit('message', {'msg':'<span class="badge grey">'+  session.get('username') + '</span> ' + message['msg'] }, room=room)
+
+
+@socketio.on('workspace', namespace='/chat')
+def workspace(message):
+    room = session.get('chat_uid')
+    emit('message', {'msg': message['msg']}, room=room)
 
 
 if __name__ == '__main__':
