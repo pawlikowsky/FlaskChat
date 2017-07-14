84e69a9d06995d2884f73feec32249615e53417c
ckp1701 <euo886@gmail.com>
2017-07-14 22:50:15 +0200

    model update

--- models.py
+++ models.py
@@ -23,7 +23,7 @@ class Chat(db.Model):
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
+    username = db.Column(db.String(80))
     email = db.Column(db.String(120), unique=True)
 
     def __init__(self, username, email=None):
